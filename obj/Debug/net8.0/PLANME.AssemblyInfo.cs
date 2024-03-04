4b1c3c1b89d5c6746c8416c67059b066de9d0cd6
filip <filip.wasiewicz11@gmail.com>
2024-03-04 21:59:54 +0100

    Learning bootstrap

--- obj/Debug/net8.0/PLANME.AssemblyInfo.cs
+++ obj/Debug/net8.0/PLANME.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PLANME")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03bb6c7a0bef22f9275bfccb3f522b517540fa31")]
 [assembly: System.Reflection.AssemblyProductAttribute("PLANME")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PLANME")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
